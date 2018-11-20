ffee744e24c65f3cadaeb505be68391dc46994c8
Ankit Shrivastava <ankitrajsrivastava06@gmail.com>
2018-11-20 13:51:44 +0530

    Check.py

---
+++ Check.py
@@ -0,0 +1,10 @@
+num = int(input("Give me  the number : "))
+check = int(input("Give me  the number to divide with : "))
+
+if num % 4 == 0:
+	print (" Number is divided by 4 ")
+elif num % 2 == 0:
+	print (" number is  even ")
+else:
+	check == 0
+	print (" number is divided by  check")
