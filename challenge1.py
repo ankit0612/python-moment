20a235c02009cbbe0f1e1619752b656fba58d7e4
Ankit Shrivastava <ankitrajsrivastava06@gmail.com>
2018-11-24 15:43:18 +0530

    Factorial of given number.
    Example:
    
    Enter number of Factorial: 6
    720

--- challenge1.py
+++ challenge1.py
@@ -1,7 +1,11 @@
-def fact(i):
-	if i == 0:
+def fact(number):
+	
+	if number== 0:
 		return 1
-	return i*fact(i-1)
-i = int(raw_input("Enter the number of Factorial: "))
+	
+	return number*fact(number-1)
 
-print fact(i)
+
+number = int(input("Enter the number of Factorial: "))
+
+print fact(number)
