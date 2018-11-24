34b392a7f279dfdd522f198441de8e411e99833d
ankit0612 <ankitrajsrivastava06@gmail.com>
2018-11-24 15:10:24 +0530

    Pattern of series of dictionary

---
+++ Dict.py
@@ -0,0 +1,7 @@
+number = int(input("Enter Number here:  "))
+for dict in range(0,number):
+    num = 1
+    for j in range(0, dict+1): 
+        print({num:num*num}, end=" ") 
+        num = num +1
+    print("\r")
