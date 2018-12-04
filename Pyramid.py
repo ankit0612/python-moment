69975d42d24ece49879dcd8b477d3e73e5ba1c78
ankit0612 <ankitrajsrivastava06@gmail.com>
2018-12-04 17:10:51 +0530

    Pyramid.

---
+++ Pyramid.py
@@ -0,0 +1,19 @@
+def triangle(number): 
+ 
+    A = 2*number - 2
+    for i in range(0, number): 
+                     
+        for B in range(0, A): 
+            print(end=" ") 
+      
+        A = A - 1
+         
+        for B in range(0, i+1): 
+        
+
+            print("* ", end="") 
+    
+        print("\r") 
+  
+number = int(input("Enter Here: "))
+triangle(number) 
