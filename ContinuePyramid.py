c2534caf16318b846b5767474bf314f7fd21985e
ankit0612 <ankitrajsrivastava06@gmail.com>
2018-12-05 17:21:48 +0530

    Pyramid numbers without re assigning.

---
+++ ContinuePyramid.py
@@ -0,0 +1,16 @@
+def contnum(Enter): 
+        
+    number = 1
+   
+    for x in range(0, Enter): 
+
+        for y in range(0, x+1):  
+            print(number, end=" ") 
+          
+            number = number + 1
+       
+        print("\r") 
+  
+Enter = int(input("Enter Here: "))
+
+contnum(Enter) 
