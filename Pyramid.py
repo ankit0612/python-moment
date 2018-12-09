35e97b0b97de5585479a9e0ce92d7b6a26a9b4f1
Ankit Shrivastava <ankitrajsrivastava06@gmail.com>
2018-12-09 17:16:13 +0530

    Update Pyramid.py
    Example:
    
    Enter Here: 10
                      * 
                     * * 
                    * * * 
                   * * * * 
                  * * * * * 
                 * * * * * * 
                * * * * * * * 
               * * * * * * * * 
              * * * * * * * * * 
             * * * * * * * * * *

--- Pyramid.py
+++ Pyramid.py
@@ -1,12 +1,12 @@
 def triangle(number): 
  
-    A = 2*number - 2
+    A = 2*number-2
     for i in range(0, number): 
                      
         for B in range(0, A): 
             print(end=" ") 
       
-        A = A - 1
+        A = A-1
          
         for B in range(0, i+1): 
         
