a4da18e048ef7c999ec35f85cdf61a1dc0b01892
Ankit Shrivastava <ankitrajsrivastava06@gmail.com>
2018-12-17 16:10:05 +0530

    Create Matrix_combination.py

---
+++ Matrix_combination.py
@@ -0,0 +1,20 @@
+
+import itertools
+r=input("Enter number of rows: ")
+#initialize blank list of r rows and any number of columns
+d=[[] for x in xrange(0,r)]
+i=0
+c=[]
+# Code block to fill in the values
+while i< len(d):
+    a=input("Enter row 1 values eg if the values are 1,2,3 enter 123: ")
+    b=str(a)
+    i=i+1
+    e=[]
+    for digits in b:
+        e.append(int(digits))
+    c.append(e)
+    #start the combination from bottom to top
+    finalList=c[::-1]
+for element in itertools.product(*finalList):
+    print element
